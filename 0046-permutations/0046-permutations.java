269f27c9d1def80b45ec6a1867faa786d625e959
Naveen Panaganti <97087329+Naveenpanaganti@users.noreply.github.com>
2026-03-20 14:30:59 +0530

    Time: 1 ms (85.24%), Space: 45.1 MB (98.4%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,24 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
         List<Integer> templist = new ArrayList<>();
-        boolean freq[] = new boolean[nums.length];
+        List<List<Integer>> ans = new ArrayList<>();
+        boolean[] freq = new boolean[nums.length];
 
-        permutations(0, nums, freq, templist, ans);
-        return ans;        
+        backtrack(0, nums, freq, templist, ans);
+        return ans;
     }
-    void permutations(int idx, int[] nums, boolean[] freq, List<Integer> templist, List<List<Integer>> ans){
+    public void backtrack(int idx, int[] nums, boolean[] freq, List<Integer> templist, List<List<Integer>> ans){
         if(templist.size() == nums.length){
             ans.add(new ArrayList(templist));
             return;
         }
-        for(int i = 0; i<nums.length; i++){
+        for(int i =0; i<nums.length; i++){
             if(!freq[i]){
                 freq[i] = true;
                 templist.add(nums[i]);
-                permutations(idx, nums, freq, templist, ans);
+                backtrack(idx+1, nums, freq, templist, ans);
                 templist.remove(templist.size()-1);
-                freq[i] = false;
+                freq[i] =false;
             }
         }
     }
