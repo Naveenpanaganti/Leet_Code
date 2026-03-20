c5f8b59f474db57a707b99964e783aa02cc07e72
Naveen Panaganti <97087329+Naveenpanaganti@users.noreply.github.com>
2026-03-20 19:45:21 +0530

    Time: 1 ms (85.24%), Space: 45.8 MB (25.63%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,28 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        boolean[] freq = new boolean[nums.length];
-        backtrack(0, nums, freq, temp, ans);
-        return ans;        
+        swapmtd(0, nums, ans);
+        return ans;
     }
-    public void backtrack(int idx, int[] nums, boolean[] freq, List<Integer> temp, List<List<Integer>> ans){
-        if(temp.size() == nums.length){
-            ans.add(new ArrayList(temp));
+    public void swapmtd(int idx, int[] nums, List<List<Integer>> ans){
+        if( idx == nums.length){                       // base case when idx is reached last element in array
+            List<Integer> temp = new ArrayList<>();        
+            for(int i =0; i< nums.length; i++){
+                temp.add(nums[i]);                         // add each elements in temporary list
+            }
+            ans.add(new ArrayList(temp));                   // after adding all elements add temp to main list ans 
             return;
         }
-        for(int i=0; i< nums.length; i++){
-            if(!freq[i]){
-                temp.add(nums[i]);
-                freq[i]=true;
-                backtrack(i+1, nums, freq, temp, ans);
-                temp.remove(temp.size()-1);
-                freq[i]= false;
-            }
+        for(int i=idx; i<nums.length; i++){                      //idx will be stable in 0 index ans i will go throught 0 to n
+            swap(i, idx, nums);                                  // swap num[idx] with all possible elements by nums[i]
+            swapmtd(idx+1,nums, ans);                            // recursion to iterate and increase idx+1
+            swap(i, idx, nums);                              // again if we swap when in backtrack it will come to normal state
         }
     }
+    public void swap(int i , int j, int[] nums){              // fun to swap to elements
+            int t = nums[i];
+            nums[i] = nums[j];
+            nums[j] = t;
+        }
+    
 }
