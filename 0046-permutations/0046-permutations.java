dce0c149415f0f949fb556cef3b0d436d0a00e8a
Naveen Panaganti <97087329+Naveenpanaganti@users.noreply.github.com>
2026-03-25 22:09:00 +0530

    Time: 1 ms (85.13%), Space: 45.6 MB (40.34%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,28 +1,25 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        swapmtd(0, nums, ans);
+        List<Integer> templist = new ArrayList<>();
+        boolean[] freq = new boolean[nums.length];
+        solu(0, nums, freq, templist, ans);
         return ans;
     }
-    public void swapmtd(int idx, int[] nums, List<List<Integer>> ans){
-        if( idx == nums.length){                       // base case when idx is reached last element in array
-            List<Integer> temp = new ArrayList<>();        
-            for(int i =0; i< nums.length; i++){
-                temp.add(nums[i]);                         // add each elements in temporary list
-            }
-            ans.add(new ArrayList(temp));                   // after adding all elements add temp to main list ans 
+    public void solu(int idx, int[] nums, boolean freq[], List<Integer> templist, List<List<Integer>> ans){
+        if(templist.size() == nums.length){
+            ans.add(new ArrayList(templist));
             return;
         }
-        for(int i=idx; i<nums.length; i++){                      //idx will be stable in 0 index ans i will go throught 0 to n
-            swap(i, idx, nums);                                  // swap num[idx] with all possible elements by nums[i]
-            swapmtd(idx+1,nums, ans);                            // recursion to iterate and increase idx+1
-            swap(i, idx, nums);                              // again if we swap when in backtrack it will come to normal state
+        for(int i = 0; i<nums.length; i++){
+            if(!freq[i]){
+                freq[i] = true;
+                templist.add(nums[i]);
+                solu(idx+1, nums, freq, templist, ans);
+                templist.remove(templist.size()-1);
+                freq[i] = false;
+            }
         }
+        return;
     }
-    public void swap(int i , int j, int[] nums){              // fun to swap to elements
-            int t = nums[i];
-            nums[i] = nums[j];
-            nums[j] = t;
-        }
-    
 }
