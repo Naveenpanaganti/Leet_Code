844307a4ab66c9c873838cf5c4f586079ff17d14
Naveen Panaganti <97087329+Naveenpanaganti@users.noreply.github.com>
2026-03-30 11:15:41 +0530

    Time: 8 ms (74.29%), Space: 64.8 MB (96.06%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        List<String> temp = new ArrayList<>();
+        Solve(0, s, temp, ans);
+        return ans;       
+    }
+    public void Solve(int idx, String s, List<String> temp, List<List<String>> ans){
+        if(idx == s.length()){
+            ans.add(new ArrayList(temp));
+            return;
+        }
+        for(int i=idx;i<s.length();i++){
+            if(ispalindrome(idx, i, s)){
+                temp.add(s.substring(idx, i+1));
+                Solve(i+1, s, temp, ans);
+                temp.remove(temp.size()-1);
+            }
+        }
+    }
+    public boolean ispalindrome(int start, int end, String s){
+        while(start <= end){
+            if(s.charAt(start++) != s.charAt(end--)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
