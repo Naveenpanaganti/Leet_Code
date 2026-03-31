14e14ea6d9d810f440b5b32cf7ecb814c52289dc
Naveen Panaganti <97087329+Naveenpanaganti@users.noreply.github.com>
2026-03-31 11:14:05 +0530

    Time: 8 ms (74.46%), Space: 65.4 MB (49.62%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -2,28 +2,30 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> ans = new ArrayList<>();
         List<String> temp = new ArrayList<>();
-        solve(0, s, temp, ans);
-        return ans;        
+        Solve(0,s, temp, ans);    
+        return ans;    
     }
-    public void solve(int idx, String s, List<String> temp, List<List<String>> ans){
+    public void Solve(int idx, String s, List<String> temp, List<List<String>> ans){
         if(idx == s.length()){
             ans.add(new ArrayList(temp));
             return;
         }
         for(int i=idx; i<s.length(); i++){
-            if(ispalindrome(idx,i,s)){
-                temp.add(s.substring(idx,i+1));
-                solve(i+1, s, temp, ans);
+            if(ispalindrome(idx, i, s, temp)){
+                temp.add(s.substring(idx, i+1));
+                Solve(i+1, s, temp, ans);
                 temp.remove(temp.size()-1);
             }
         }
     }
-    public boolean ispalindrome(int start, int end, String s){
-        while(start <=  end){
-            if(s.charAt(start++) != s.charAt(end--)){
-                return false;
-            }
+    public boolean ispalindrome(int start, int end, String s, List<String> temp){
+        while(start <= end){
+            if(s.charAt(start) != s.charAt(end))
+            return false;
+            start++;
+            end--;
         }
         return true;
+
     }
 }
