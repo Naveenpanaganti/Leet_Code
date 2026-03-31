415c70cb3824a4bd856dfd8118941252f54420aa
Naveen Panaganti <97087329+Naveenpanaganti@users.noreply.github.com>
2026-03-31 14:25:36 +0530

    Time: 3 ms (42.32%), Space: 46.7 MB (55.38%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,39 +1,42 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
-        List<String> board = new ArrayList<>();
-        String s = ".".repeat(n);
-        for(int i=0; i<n; i++){
-            board.add(s);
+        List<String> temp = new ArrayList<>();
+        String s =".".repeat(n);
+        for(int i=0;i<n;i++){
+            temp.add(s);
         }
         int[] leftrow = new int[n];
-        int[] upperdiaganol = new int[2*n-1];
-        int[] lowerdiaganol = new int[2*n-1];
-        solve(0, board, ans, n, leftrow, upperdiaganol, lowerdiaganol);
-        return ans;        
+        int[] upperdiaganol = new int[n*2-1];
+        int[] lowerdiaganol = new int[n*2-1];
+        Solve(0, n, leftrow, upperdiaganol, lowerdiaganol, temp, ans);
+        return ans;
     }
-    public void solve(int col, List<String> board, List<List<String>> ans, int n, int[] leftrow, int[] upperdiaganol, int[] lowerdiaganol){
-        if(col == n){
-            ans.add(new ArrayList(board));
+    public void Solve(int col, int n, int[] leftrow, int[] upperdiaganol, int[] lowerdiaganol, List<String> temp, List<List<String>> ans){
+        if(col >=  n){
+            ans.add(new ArrayList<>(temp));
             return;
         }
         for(int row=0; row<n; row++){
-            if(leftrow[row] == 0 && upperdiaganol[row+col] == 0 && lowerdiaganol[n-1+col-row] == 0){
-                char[] rowarray = board.get(row).toCharArray();
+            if(leftrow[row] == 0 && upperdiaganol[row+col] == 0 && lowerdiaganol[(n-1)+(col-row)] == 0){
+                char[] rowarray = temp.get(row).toCharArray();
                 rowarray[col] = 'Q';
-                board.set(row, new String(rowarray));
+                temp.set(row, new String(rowarray));
 
-                leftrow[row]=1;
-                upperdiaganol[row+col]=1;
-                lowerdiaganol[n-1+col-row] = 1;
+                leftrow[row] =1;
+                upperdiaganol[row+col] = 1;
+                lowerdiaganol[(n-1)+(col-row)] =1;
 
-                solve(col+1, board, ans, n, leftrow, upperdiaganol, lowerdiaganol);
-                rowarray[col] ='.';
-                board.set(row, new String(rowarray));
-                leftrow[row]=0;
-                upperdiaganol[row+col]=0;
-                lowerdiaganol[n-1+col-row] = 0;
+                Solve(col+1, n, leftrow, upperdiaganol, lowerdiaganol, temp, ans);
+                rowarray[col]='.';
+                temp.set(row, new String(rowarray));
+                leftrow[row] =0;
+                upperdiaganol[row+col] = 0;
+                lowerdiaganol[(n-1)+(col-row)] = 0;
+
             }
+
+
         }
     }
 }
