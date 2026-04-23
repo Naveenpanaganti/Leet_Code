ab056d2c8c99f216c582ed0aab2bc29e3ce321d7
Naveen Panaganti <97087329+Naveenpanaganti@users.noreply.github.com>
2026-04-23 10:48:21 +0530

    Time: 2 ms (86.26%), Space: 47.9 MB (63.96%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        //int jump =0;
         int n = nums.length;
-        int maxidx =0;
+        int maxidx = 0;
         for(int i=0; i<n; i++){
-            if(i>maxidx) return false;
-            maxidx = Math.max(maxidx,i+nums[i]);
+            if(i>maxidx){
+                return false;
+            }
+            maxidx = Math.max(maxidx, i+nums[i]);
         }
         return true;
     }
